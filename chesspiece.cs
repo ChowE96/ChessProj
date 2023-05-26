64473de3ad1d031c32cd6fc282fa257c64bf8290
Edison <tecone1010@gmail.com>
2023-05-26 10:40:25 -0400

    Setting up for pieve movement

--- chesspiece.cs
+++ chesspiece.cs
@@ -2,14 +2,16 @@ namespace Chess {
     public class ChessPiece {
         private string icon;
         private string name;
-        private int x;
-        private int y;
+        private string type;
 
-        public void moveType() {}
+        public void moveType(string type) {
+            
+        }
 
         public ChessPiece(string icon, string name) {
             this.icon = icon;
             this.name = name;
+            this.type = name;
         }
 
         //These are so cool
