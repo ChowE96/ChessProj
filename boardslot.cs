c4a7ebe8a30c69ff267983bef4f71ef01cb43cd8
Edison <tecone1010@gmail.com>
2023-05-28 10:25:59 -0400

    New default constructor

--- boardslot.cs
+++ boardslot.cs
@@ -6,11 +6,16 @@ namespace Chess {
             get => piece;
             set => piece = value;
         }
-        public void empty() {
-            piece = new ChessPiece();
+
+        public void empty() { 
+            piece = new ChessPiece(); 
         }
         public bool isEmpty() {
             return( piece.Name == "" );
+        }
+
+        public BoardSlot() {
+            empty();
         }
         public BoardSlot(string icon, string name) {
             piece = new ChessPiece(icon, name);
