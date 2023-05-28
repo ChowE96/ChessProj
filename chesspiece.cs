b86c2f9c8107a9978753d646701ca318485b5e97
Edison <tecone1010@gmail.com>
2023-05-28 11:47:33 -0400

    Removed redundant logic

--- chesspiece.cs
+++ chesspiece.cs
@@ -2,36 +2,6 @@ namespace Chess {
     public class ChessPiece {
         private string icon;
         private string name;
-        private PieceType type;
-        enum PieceType {
-            Pawn,
-            Bishop,
-            Knight,
-            Rook,
-            Queen,
-            King
-        }
-
-        public void moveType() {
-            switch (type) {
-            case PieceType.Pawn:
-                break;
-            case PieceType.Bishop:
-                break;
-            case PieceType.Knight:
-                break;
-            case PieceType.Rook:
-                break;
-            case PieceType.Queen:
-                break;
-            case PieceType.King:
-                break;
-            //Default
-            default:
-                Console.WriteLine("No such piece exists");
-                break;
-            }
-        }
 
         public ChessPiece() {
             this.icon = "  ";
@@ -40,7 +10,6 @@ namespace Chess {
         public ChessPiece(string icon, string name) {
             this.icon = icon;
             this.name = name;
-            this.type = Enum.Parse<PieceType>(name);
         }
 
         //These are so cool
