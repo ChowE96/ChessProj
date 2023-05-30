a60abd1c21665c00c284337e51ed2aeb3069dddb
Edison <tecone1010@gmail.com>
2023-05-30 03:02:37 -0400

    Streamlined outputting a piece on the board

--- boardslot.cs
+++ boardslot.cs
@@ -1,10 +1,15 @@
 namespace Chess {
     public class BoardSlot {
         private ChessPiece piece;
+        private bool isSelected = false;
 
         public ChessPiece Piece {
             get => piece;
             set => piece = value;
+        }
+        public bool IsSelected {
+            get => isSelected;
+            set => isSelected = value;
         }
 
         public void empty() { 
@@ -13,12 +18,20 @@ namespace Chess {
         public bool isEmpty() {
             return( piece.Name == "" );
         }
+        public override string ToString() {
+            if (isSelected) { return "{" + piece.Icon + "}"; }
+            else { return "[" + piece.Icon + "]"; }
+        }
 
+        //Constructors
         public BoardSlot() {
             empty();
         }
         public BoardSlot(string icon, string name) {
             piece = new ChessPiece(icon, name);
+        }
+        public BoardSlot(string id) {
+            
         }
     }
 }
