a000498a49efdcf2591406fb4360dd4111059fe3
Edison <tecone1010@gmail.com>
2023-05-30 03:03:03 -0400

    Changed logic to include selection

--- game.cs
+++ game.cs
@@ -3,13 +3,23 @@ namespace Chess {
         //This is the main game loop!
         public static void run() {
             Board board = new Board();
-            board.fillBoard();
+            // board.fillBoard();
+            board.fillEmptyBoard();
 
+            // Spawning in objects
+            board.spawnPiece("2",5,5);
+            
             while(!board.isCheckmate()) {
 
                 board.drawBoard();
 
-                board.movePiece();
+                if (board.selectPiece()) {
+                    
+                    board.drawBoard();
+
+                    board.movePiece();
+
+                }
                 
             }
         }
