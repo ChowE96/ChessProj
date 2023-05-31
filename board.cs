31614b51709f259fdf7b0e871f83f58fc0db6eeb
Edison <tecone1010@gmail.com>
2023-05-31 12:25:04 -0400

    Added player color

--- board.cs
+++ board.cs
@@ -3,7 +3,7 @@ using System.Collections;
 namespace Chess {
     public class Board {
         private BoardSlot[,] boardXY = new BoardSlot[8, 8];
-        enum PieceType {
+        private enum PieceType {
             Pawn,
             Bishop,
             Knight,
@@ -11,8 +11,9 @@ namespace Chess {
             Queen,
             King
         }
-        int[]? currSelection = new int[2];
-        string turn; //Determines who's turn it is
+        private int[] currSelection = new int[2];
+        private string turn; //Determines who's turn it is
+
 
         //Constructor
         public BoardSlot[,] BoardXY {
@@ -26,28 +27,28 @@ namespace Chess {
                 for(int y = 0; y < boardXY.GetLength(1); y++) {
                     
                     //Pawns
-                    if (x == 1) { boardXY[x,y] = new BoardSlot("BP","Pawn"); }
-                    if (x == 6) { boardXY[x,y] = new BoardSlot("WP","Pawn"); }
+                    if (x == 1) { boardXY[x,y] = new BoardSlot("BP","Pawn","Black"); }
+                    if (x == 6) { boardXY[x,y] = new BoardSlot("WP","Pawn","White"); }
 
                     //Bishops
-                    if ((x == 0 && y == 2) || (x == 0 && y == 5)) { boardXY[x,y] = new BoardSlot("BB","Bishop"); }
-                    if ((x == 7 && y == 2) || (x == 7 && y == 5)) { boardXY[x,y] = new BoardSlot("WB","Bishop"); }
+                    if ((x == 0 && y == 2) || (x == 0 && y == 5)) { boardXY[x,y] = new BoardSlot("BB","Bishop","Black"); }
+                    if ((x == 7 && y == 2) || (x == 7 && y == 5)) { boardXY[x,y] = new BoardSlot("WB","Bishop","White"); }
 
                     //Knights
-                    if ((x == 0 && y == 1) || (x == 0 && y == 6)) { boardXY[x,y] = new BoardSlot("BN","Knight"); }
-                    if ((x == 7 && y == 1) || (x == 7 && y == 6)) { boardXY[x,y] = new BoardSlot("WN","Knight"); }
+                    if ((x == 0 && y == 1) || (x == 0 && y == 6)) { boardXY[x,y] = new BoardSlot("BN","Knight","Black"); }
+                    if ((x == 7 && y == 1) || (x == 7 && y == 6)) { boardXY[x,y] = new BoardSlot("WN","Knight","White"); }
 
                     //Rooks
-                    if ((x == 0 && y == 0) || (x == 0 && y == 7)) { boardXY[x,y] = new BoardSlot("BR","Rook"); }
-                    if ((x == 7 && y == 0) || (x == 7 && y == 7)) { boardXY[x,y] = new BoardSlot("WR","Rook"); }
+                    if ((x == 0 && y == 0) || (x == 0 && y == 7)) { boardXY[x,y] = new BoardSlot("BR","Rook","Black"); }
+                    if ((x == 7 && y == 0) || (x == 7 && y == 7)) { boardXY[x,y] = new BoardSlot("WR","Rook","White"); }
 
                     //Queens
-                    if ((x == 0 && y == 3)) { boardXY[x,y] = new BoardSlot("BQ","Queen"); }
-                    if ((x == 7 && y == 3)) { boardXY[x,y] = new BoardSlot("WQ","Queen"); }
+                    if ((x == 0 && y == 3)) { boardXY[x,y] = new BoardSlot("BQ","Queen","Black"); }
+                    if ((x == 7 && y == 3)) { boardXY[x,y] = new BoardSlot("WQ","Queen","White"); }
 
                     //Kings
-                    if ((x == 0 && y == 4)) { boardXY[x,y] = new BoardSlot("BK","King"); }
-                    if ((x == 7 && y == 4)) { boardXY[x,y] = new BoardSlot("WK","King"); }
+                    if ((x == 0 && y == 4)) { boardXY[x,y] = new BoardSlot("BK","King","Black"); }
+                    if ((x == 7 && y == 4)) { boardXY[x,y] = new BoardSlot("WK","King","White"); }
 
                     //Empty Spaces
                     //TODO: Recognize if a spot is empty without a piece and fill that in automatically
@@ -83,11 +84,11 @@ namespace Chess {
         }
         
         //Spawns in a chess piece
-        public void spawnPiece(string id, int x, int y) {
-            boardXY[x,y] = new BoardSlot("WQ","Queen");
+        public void spawnPiece(string icon, string name, string color, int x, int y) {
+            boardXY[x,y] = new BoardSlot(icon,name,color);
         }
 
-        //Select pieces and give coordinates they can move to
+        //Select pieces using coordinates and give coordinates they can move to
         public bool selectPiece() {
             //Debug
             //Console.WriteLine(x + " " + y);
@@ -103,6 +104,17 @@ namespace Chess {
                     currSelection[0] = x;
                     currSelection[1] = y;
                     boardXY[x,y].IsSelected = true;
+                    
+                    //Select all the squares it can move to
+                    for(int i = 0; i <= 7; i++) {
+                        for(int j = 0; j <= 7; j++) {
+                            if ( validMove(x,y,i,j) ) {
+                                // Console.Write("(" + (char)(j + 97) + "," + (8 - i) + ")");
+                                boardXY[i,j].IsSelected = true;
+                            }
+                        }
+                    }
+                    // Console.WriteLine();
                     return true;
                 }
                 else {
@@ -116,6 +128,16 @@ namespace Chess {
             }
         }
         
+        //Deselect all slots
+        public void deselectAll() {
+            for(int i = 0; i <= 7; i++) {
+                for(int j = 0; j <= 7; j++) {
+                    currSelection = new int[2];
+                    boardXY[i,j].IsSelected = false;  
+                }
+            }
+        }
+
         //Move pieces
         public void movePiece() {
             Console.WriteLine("Where do you want to move that piece: ");
@@ -128,33 +150,28 @@ namespace Chess {
             try {
                 if(x == toX && y == toY) {
                     Console.WriteLine("You deselected: " + boardXY[x,y].Piece.Name);
-                    currSelection = new int[2];
-                    boardXY[x,y].IsSelected = false;
                     return;
                 }
                 if(validMove(x,y,toX,toY)) {
                     if (boardXY[toX,toY].isEmpty()) {
                         boardXY[toX,toY].Piece = boardXY[x,y].Piece;
                         boardXY[x,y].empty();
+                        return;
                     }
                     else {
                         boardXY[toX,toY].empty();
                         boardXY[toX,toY].Piece = boardXY[x,y].Piece;
                         boardXY[x,y].empty();
+                        return;
                     }
-                    currSelection = new int[2];
-                    boardXY[x,y].IsSelected = false;   
                 }
                 else {
                     Console.WriteLine("You cannot move to that location");
-                    currSelection = new int[2];
-                    boardXY[x,y].IsSelected = false;
+                    return;
                 }
             }
             catch {
                 Console.WriteLine("You cannot move to that location");
-                currSelection = new int[2];
-                boardXY[x,y].IsSelected = false;
                 return;
             }
         }
@@ -173,8 +190,8 @@ namespace Chess {
             int offset = toX - x;
                 switch (type) {
                 case PieceType.Pawn:
-                    if ( (toX == x + 1)
-                        || (toX == x - 1) ) {
+                    if ( (toX == x + 1) && boardXY[x,y].Piece.Color == "Black" && (toY == y)
+                        || (toX == x - 1) && boardXY[x,y].Piece.Color == "White" && (toY == y)) {
                         return true;
                     }
                     break;
@@ -186,26 +203,12 @@ namespace Chess {
                     break;
                 case PieceType.Knight:
                     bool NAlg(int i, int j) {
-                        // if ( (Math.Abs(x - i) == 1) && ((Math.Abs(y = j) == 2)) 
-                        //     || (Math.Abs(x - i) == 2) && ((Math.Abs(y = j) == 1)) ) {
-                        //     return true;
-                        // }
                         if ( ((i == x - 2) || (i == x + 2)) && ((j == y + 1) || (j == y - 1)) 
                         || ((j == y - 2) || (j == y + 2)) && ((i== x + 1) || (i == x - 1)) ) {
                             return true;
                         }
                         else { return false; }
                     }                   
-
-                    //Debug
-                    // for(int i = 0; i <= 7; i++) {
-                    //     for(int j = 0; j <= 7; j++) {
-                    //         if ( NAlg(i,j) ) {
-                    //             Console.Write("(" + (char)(j + 97) + "," + (8 - i) + ")");
-                    //         }
-                    //     }
-                    // }
-                    // Console.WriteLine();
 
                     if ( NAlg(toX,toY) ) {
                         return true;
