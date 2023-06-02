28c707a090528fcfc9cfb437d30209f2ebb5d845
Edison <tecone1010@gmail.com>
2023-06-02 00:30:45 -0400

    All the enums!

---
+++ info.cs
@@ -0,0 +1,15 @@
+namespace Chess {
+    public enum PieceName {
+        Pawn,
+        Bishop,
+        Knight,
+        Rook,
+        Queen,
+        King
+    }
+
+    public enum PieceColor {
+        White,
+        Black
+    }
+}
